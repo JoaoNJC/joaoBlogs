9aa0eae98d4698a9d29c7db5ec88b3c361f682be
João Camba <joaonoeljose@gmail.com>
2023-07-18 09:32:02 +0100

    Navbar set up. Mark down files added.

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@
 
 export default function Home() {
   return (
-    <main>
-
+    <main className="px-6 mx-auto">
+      <p>
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I'm <span className="font-bold">João</span>
+        </span>
+      </p>
     </main>
   )
 }
